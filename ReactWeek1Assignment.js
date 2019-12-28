e9a701c5fa9f8b2f27b05bd917098483d6c0b88f
Nathan Chuluda <nchuluda@gmail.com>
2019-12-28 11:36:20 -0500

    js

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,16 @@
+const Student = {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+const Bootcamp {
+    constructor(name, level, students = []) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+}
+
