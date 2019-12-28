c70cb72366e8ed83e9277de36f6eadf5b3b31544
Nathan Chuluda <nchuluda@gmail.com>
2019-12-28 12:11:55 -0500

    3

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -1,4 +1,4 @@
-const Student = {
+class Student {
     constructor(name, email, community) {
         this.name = name;
         this.email = email;
@@ -6,19 +6,26 @@ const Student = {
     }
 }
 
-const Bootcamp {
+class Bootcamp {
     constructor(name, level, students = []) {
         this.name = name;
         this.level = level;
         this.students = students;
     }
-}
-
-function registerStudent(studentToRegister) {
+    registerStudent(studentToRegister) {
     if ( Bootcamp.students.includes(studentToRegister)) {
         console.log("This email has already been registered.");
     } else {
         Bootcamp.students.push(studentToRegister);
+        console.log(`Registering ${this.email} to the bootcamp for Web Dev fundamentals. `)
+    }
+    return Bootcamp.students;
     }
 }
+
+let student1 = new Student("David", "david@gmail.com", "ONCE");
+let student2 = new Student("Charles", "charles@gmail.com", "ONCE");
+let student3 = new Student("Minae", "minae@gmail.com", "ONCE");
+
+
 
